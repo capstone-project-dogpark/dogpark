a21e4ae93f180d5b0c9deee0c51c663e1a5cc233
David Santos <dsantos4@cnm.edu>
2022-11-09 11:51:40 -0700

    added activation.controller.ts

---
+++ backend/src/apis/sign-up/activation.controller.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from "express";
+import { Profile , selectProfileByProfileActivationToken, updateProfile} from '../../utils/models/Profile'
+
+
+export async function activationController (request: Request, response: Response): Promise<Response>{
+    try {
+        const {activation} = request.params
+        const profile = await selectProfileByProfileActivationToken(activation)
+        console.log(profile)
+
+        const activationFailed = (): Response => response.json({
+            status: 400,
+            data: null,
+            message: 'Account activation has failed. Have you already activated this account'
+        })
+
+        const activationSucceeded = async (profile: Profile): Promise<Response> => {
+            const updatedProfile = {...profile, profileActivationToken: null}
+            console.log(updatedProfile)
+            await updateProfile(updatedProfile)
+            return response.json({
+                status: 200,
+                data: null,
+                message: 'Account activation was successful'
+            })
+        }
+        return (profile != null) ? await activationSucceeded(profile) : activationFailed()
+    } catch (error:any) {
+            return response.json ({status: 500, data: null, message: error.message})
+        }
+}
--- backend/src/utils/models/Profile.ts
+++ backend/src/utils/models/Profile.ts
@@ -15,3 +15,14 @@ export async function insertProfile(profile:Profile) {
     await sql`Insert Into profile(profile_id, profile_about_pet, profile_activation_token, profile_at_handle, profile_email, profile_hash, profile_image) VALUES (gen_random_uuid(),${profileAboutPet},${profileActivationToken},${profileAtHandle},${profileEmail},${profileHash},${profileImage})`
     return 'profile successfully created'
 }
+
+export async function selectProfileByProfileActivationToken (profileActivationToken: string): Promise<Profile|null> {
+    const result = <Profile[]>await sql`SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle,profile_email,profile_hash,profile_image FROM  profile WHERE profile_activation_token = ${profileActivationToken}`
+    return result?.length === 1 ? result[0] : null
+}
+
+export async function updateProfile (profile: Profile): Promise<string> {
+    const {profileId, profileActivationToken, profileAtHandle, profileEmail, profileHash, profileImage} = profile
+    await sql `UPDATE profile SET profile_activation_token = ${profileActivationToken}, profile_at_handle = ${profileAtHandle}, profile_email = ${profileEmail}, profile_hash = ${profileHash}, profile_image = ${profileImage} WHERE profile_id = ${profileId}`
+    return 'Profile successfully updated'
+}
