e246de361672a968d9c6cadd8e8551bf9c501809
David Santos <dsantos4@cnm.edu>
2022-11-10 16:52:08 -0700

    updated Pratial/Whole async in Profile.ts & status.ts

--- backend/src/utils/models/Profile.ts
+++ backend/src/utils/models/Profile.ts
@@ -1,5 +1,12 @@
 import {sql} from "../database.utils";
 
+export interface PartialProfile {
+    profileId: string|null
+    profileAtHandle: string
+    profileAboutPet: string
+    profileEmail: string
+    profileImage: string
+}
 
 export interface Profile {
     profileId: string|null,
@@ -17,6 +24,11 @@ export async function insertProfile(profile:Profile) {
     return 'profile successfully created'
 }
 
+export async function selectPartialProfileByProfileId(profileId: string): Promise<Profile|null> {
+    const result = <Profile[]>await sql `SELECT profile_id, profile_at_Handle, profile_about_pet, profile_email, profile_image from profile WHERE profile_id = ${profileId}`
+    return result?.length === 1 ? result[0] : null
+}
+
 export async function selectProfileByProfileActivationToken (profileActivationToken: string): Promise<Profile|null> {
     const result = <Profile[]>await sql`SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle,profile_email,profile_hash,profile_image FROM  profile WHERE profile_activation_token = ${profileActivationToken}`
     return result?.length === 1 ? result[0] : null
@@ -32,4 +44,9 @@ export async function selectProfileByProfileEmail (profileEmail: string): Promis
     const result = <Profile[]>await sql `SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle, profile_email, profile_hash, profile_image FROM profile WHERE profile_email = ${profileEmail}`
     return result?.length === 1 ? result[0] : null
 
+}
+
+export async function selectWholeProfileByProfileId (profileId: string): Promise<Profile|null> {
+const result = <Profile[]>await sql `SELECT profile_id, profile_activation_token, profile_at_handle, profile_about_pet, profile_email, profile_hash, profile_image FROM profile WHERE profile_id = ${profileId}`
+return result?.length === 1 ? result[0] : null
 }
