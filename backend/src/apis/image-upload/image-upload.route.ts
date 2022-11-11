009b9e089f5e86c0d4a82a1d6a8b144d96411858
David Santos <dsantos4@cnm.edu>
2022-11-11 10:04:29 -0700

    change image-upload.route.ts

--- backend/src/apis/image-upload/image-upload.route.ts
+++ backend/src/apis/image-upload/image-upload.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 import { imageUploader } from '../../utils/controllers/multer.controller'
-import { imageUploadController } from 'image-upload.controller'
+import { imageUploadController } from './image-upload.controller'
 
 export const ImageUploadRoute = Router()
 
