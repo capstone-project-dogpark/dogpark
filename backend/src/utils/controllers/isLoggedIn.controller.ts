7cf325b3a699989b0270b590da806619c98c7ba3
Aaron Murillo <amurillo11@cnm.edu>
2022-11-10 19:49:25 -0700

    completed isLoggedIn.controller.ts file

--- backend/src/utils/controllers/isLoggedIn.controller.ts
+++ backend/src/utils/controllers/isLoggedIn.controller.ts
@@ -1,8 +1,49 @@
+import { NextFunction, Request, Response } from "express"
+import { verify, VerifyErrors} from "jsonwebtoken";
+import {Status} from "../interfaces/Status";
+import {Profile} from '../models/Profile'
+import {IncomingHttpHeaders} from 'http'
 
+export function isLoggedIn (request: Request, response: Response, next: NextFunction): any {
+    const status: Status = {status: 400, message: 'Please log in', data: null}
 
+const sessionProfile = (request: Request): Profile | undefined => request.session?.profile ?? undefined
+console.log(request.sessionID)
 
+const signature = (request: Request): string => request.session?.signature ?? 'no signature'
 
-export function isLoggedIn (request: Request, response: Response): any {
-    const status
+const isSessionsActive = (isProfileActive: Profile | undefined): boolean => (isProfileActive !== undefined)
+
+const getJwtTokenFromHeader = (header: IncomingHttpHeaders): string | undefined => {
+    return header.authorization
 }
 
+const unverifiedJwtToken: string | undefined = getJwtTokenFromHeader (request.headers)
+
+// const isJwtValid: boolean|void = unverifiedJwtToken
+//   ? verify(
+//         unverifiedJwtToken,
+//         signature(request),
+//         {maxAge: "3hr"},
+//
+//     )
+//   : false;
+
+const isJwtValid = (unverifiedJwtToken: string | undefined): boolean => {
+    if (unverifiedJwtToken === undefined) {
+        return false;
+    }
+const result: unknown = verify(
+    unverifiedJwtToken,
+    signature(request),
+    {maxAge: '3hr' },
+    (error: VerifyErrors | null): boolean => error == null
+) as unknown
+
+return result as boolean
+}
+
+if(isJwtValid(unverifiedJwtToken) && isSessionsActive(sessionProfile(request))) {
+    return next()
+}
+isJwtValid(unverifiedJwtToken) && isSessionsActive(sessionProfile(request)) ? next() : response.json(status)
