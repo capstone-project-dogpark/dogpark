484f3b7bd07ce3ac5b25c48141ce01b383daae9a
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-10 20:41:55 -0800

    added missing code into Profile.ts

--- backend/src/utils/models/Profile.ts
+++ backend/src/utils/models/Profile.ts
@@ -1,5 +1,12 @@
 import {sql} from "../database.utils";
 
+export interface PartialProfile {
+    profileId: string|null
+    profileAtHandle: string
+    profileAboutPet: string|null
+    profileEmail: string
+    profileImage: string|null
+}
 
 export interface Profile {
     profileId: string|null,
@@ -17,19 +24,28 @@ export async function insertProfile(profile:Profile) {
     return 'profile successfully created'
 }
 
+export async function selectPartialProfileByProfileId(profileId: string): Promise<Profile|null> {
+    const result = <Profile[]>await sql'SELECT profile_id, profile_at_handle, profile_about_pet, profile_email, profile_image FROM profile WHERE profile_id = ${profileId}`
+        return result?.length === 1 ? result[0] : null
+}
+
 export async function selectProfileByProfileActivationToken (profileActivationToken: string): Promise<Profile|null> {
     const result = <Profile[]>await sql`SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle,profile_email,profile_hash,profile_image FROM  profile WHERE profile_activation_token = ${profileActivationToken}`
     return result?.length === 1 ? result[0] : null
+}
+
+export async function selectProfileByProfileEmail (profileEmail: string): Promise<Profile|null> {
+    const result = <Profile[]>await sql `SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle, profile_email, profile_hash, profile_image FROM profile WHERE profile_email = ${profileEmail}`
+    return result?.length === 1 ? result[0] : null
+}
+
+export async function selectWholeProfileByProfileId(profileId: string): Promise<Profile|null> {
+    const result = <Profile[]>await sql 'SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle, profile_email, profile_hash, profile_image FROM profile WHERE profile_id = ${profileId}`
+        return result?.length === 1 ? result [0] : null
 }
 
 export async function updateProfile (profile: Profile): Promise<string> {
     const {profileId, profileActivationToken, profileAtHandle, profileEmail, profileHash, profileImage} = profile
     await sql `UPDATE profile SET profile_activation_token = ${profileActivationToken}, profile_at_handle = ${profileAtHandle}, profile_email = ${profileEmail}, profile_hash = ${profileHash}, profile_image = ${profileImage} WHERE profile_id = ${profileId}`
     return 'Profile successfully updated'
-}
-
-export async function selectProfileByProfileEmail (profileEmail: string): Promise<Profile|null> {
-    const result = <Profile[]>await sql `SELECT profile_id, profile_about_pet, profile_activation_token, profile_at_handle, profile_email, profile_hash, profile_image FROM profile WHERE profile_email = ${profileEmail}`
-    return result?.length === 1 ? result[0] : null
-
 }
