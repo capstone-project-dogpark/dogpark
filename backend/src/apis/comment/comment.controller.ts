6895717a71bf4fc56cf83410995d974ad3e6a4d8
Aaron Murillo <amurillo11@cnm.edu>
2022-11-14 15:41:06 -0700

    adding comment files

---
+++ backend/src/apis/comment/comment.controller.ts
@@ -0,0 +1,80 @@
+import {Request, Response NextFunction} from "express";
+import {
+    insertComment
+    selectAllComments
+    selectCommentByCommentId
+    selectCommentByCommentProfileId
+    comment
+} from "../../utils/models/comment";
+import { Status } from '../../utils/interfaces/Status'
+import {Profile, selectWholeCommentByCommentId} from '../../utils/models/comment'
+
+export async function getAllCommentsController (request: Request, response: Response, nextFunction: NextFunction): Promise<Response<Status>> {
+    try {
+        const data = await selectAllComments()
+        const status: Status = {status: 200, message: null, data}
+        return response.json(status)
+    }
+        catch (error) {
+            return response.json({
+                status: 500,
+                message: '',
+                data: []
+            })
+        }
+    }
+
+
+export async function getCommentByCommentIdController (request: Request, response: Response, nextFunction: NextFunction): Promise<Response<Status>> {
+    try {
+        const {commentProfileId} = request.params
+        const data = await selectCommentByCommentId (commentProfileId)
+        const status: Status = { status: 200, message: null, data}
+        return response.json(status)
+    } catch (error) {
+        return response.json({
+            status: 500,
+            message: '',
+            data: []
+        })
+    }
+}
+
+export async function getCommentByCommentIdController (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const { commentId } = request.params
+        const data = await selectCommentByCommentId(commentId)
+        return response.json({status: 200, message: null, data})
+    } catch (error) {
+        return response.json({
+            status: 500,
+            message: '',
+            data: null
+        })
+    }
+}
+
+export async function commentComment (request: Request, response: Response): Promise<Response<Status>> {
+    try {
+        const profile: Profile = request.session.profile as Profile
+        const commentProfileId: string = profile.profileId as string
+
+        const comment = {
+            commentId: null,
+            commentProfileId,
+            commentDate: null
+        }
+        const result = await insertComment(comment)
+        const status: Status = {
+            status: 200,
+            message: result,
+            data: null
+        }
+        return response.json(status)
+    }   catch (error) {
+        return response.json({
+            status: 500,
+            message: 'Error creating comment try again later',
+        })
+    }
+}
