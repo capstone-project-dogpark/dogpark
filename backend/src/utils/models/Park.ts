d0aaa8c9495f8dbd7d3cb13d5bdbd3fe59ed1536
Aaron Murillo <amurillo11@cnm.edu>
2022-11-14 17:03:29 -0700

    updating park.ts file

--- backend/src/utils/models/Park.ts
+++ backend/src/utils/models/Park.ts
@@ -9,7 +9,12 @@ export interface Park {
 }
 export async function insertPark(park: Park): Promise<string> {
     const {parkId, parkAddress, parkLat, parkLng, parkName} = park
-    await sql 'INSERT INTO park (park_id, park_address, park_lat, park_lng, park_name) VALUES (gen_random_uuid), ${park_id}, ${park_address}, ${park_lat}, ${park_lng}, ${park_name}, NOW())'
+    await sql `INSERT INTO park (park_id, park_address, park_lat, park_lng, park_name) VALUES (gen_random_uuid), ${parkId}, ${parkAddress}, ${parkLat}, ${parkLng}, ${parkName}`
     return 'Park created successfully'
 }
 
+
+
+export async function getParkByDistance(userLong:number, userLat:number, distance:number):Promise<Park[]> {
+    return sql<Park[]>`SELECT parkId, parkAddress, parkLat, parkLng, parkName FROM park WHERE haversine(${})`
+}
