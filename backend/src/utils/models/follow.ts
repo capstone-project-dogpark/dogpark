a34fb2e74e29446d4c88e0ced7ab7f5159c2471c
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-15 17:14:03 -0800

    saving changes made to follow files

--- backend/src/utils/models/follow.ts
+++ backend/src/utils/models/follow.ts
@@ -8,15 +8,22 @@ export interface Follow {
 
 }
 
-export async function  followProfile (follow: Follow): Promise<string> {
+export async function  insertFollow (follow: Follow): Promise<string> {
     const {followProfileId, followerProfileId} = follow
     await sql `INSERT INTO follow (follow_profile_id, follower_profile_id, follow_date) VALUES(${followProfileId}, ${followerProfileId}, NOW())`
     return 'Follow successful'
 }
 
-export async function unfollowProfile(follow: Follow): Promise<string> {
+export async function deleteFollow(follow: Follow): Promise<string> {
     const {followerProfileId, followProfileId} = follow
     await sql `DELETE FROM follow WHERE follower_profile_id = ${followerProfileId} AND follow_profile_id = ${followProfileId}`
     return 'Unfollow Successful'
 }
+
+export async function selectFollowByFollowProfileId
+
+export async function selectFollowsByFollowProfileId (followProfileId: string): Promise<Follow[]> {
+    return <Follow[]> await sql `SELECT follow_profile_id, follower_profile_id, follow_date FROM follow WHERE follow_profile_id = ${followProfileId}`
+}
+
 
