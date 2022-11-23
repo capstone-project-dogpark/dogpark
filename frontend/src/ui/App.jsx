5a3bda6dab792ce711e11a3d909a770aa1239bdd
David Santos <dsantos4@cnm.edu>
2022-11-23 08:53:40 -0700

    added Navbar.jsx & route

--- frontend/src/ui/App.jsx
+++ frontend/src/ui/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 import { Home } from './Home.jsx'
 import { FourOhFour } from './FourOhFour.jsx'
+import {Navbar} from "./Navbar.jsx";
 
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
                 <Routes>
                     <Route  path='/' element={<Home />} />
                     <Route path={"*"} element={<FourOhFour />} />
+                    <Route path={"/Navbar"} element={<Navbar />} />
                 </Routes>
             </BrowserRouter>
         </>
--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import {Button, Col, Container, Row, Nav} from "react-bootstrap";
+import {Navbar} from "./Navbar.jsx";
 
 export function Home() {
     return (
         <>
-            <h1>Home</h1>
+            <Navbar></Navbar>
         </>
     )
 }
