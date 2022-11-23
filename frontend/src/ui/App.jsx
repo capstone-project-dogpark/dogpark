ac76d76de465097d2fce0234ac1f7af410fbc1e2
David Santos <dsantos4@cnm.edu>
2022-11-23 15:29:52 -0700

    created static my friends page & added img placeholder

--- frontend/src/ui/App.jsx
+++ frontend/src/ui/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { FourOhFour } from './FourOhFour.jsx'
 import {Navigation} from "./components/Navbar.jsx";
 import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import {MyFriendsPage} from "./MyFriendsPage.jsx";
 
 
 export function App() {
@@ -16,6 +17,7 @@ export function App() {
                 <Routes>
                     <Route  path='/' element={<Home />} />
                     <Route path={"*"} element={<FourOhFour />} />
+                    <Route path='MyFriendsPage' element={<MyFriendsPage />} />
                 </Routes>
             </BrowserRouter>
         </>
