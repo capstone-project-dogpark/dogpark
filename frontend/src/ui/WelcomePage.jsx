4ed73c67da4ec837f5a67c9fb2508968914651c7
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-23 16:00:52 -0800

    made changes to welcome page while in develop branch, added placeholder photo

--- frontend/src/ui/WelcomePage.jsx
+++ frontend/src/ui/WelcomePage.jsx
@@ -1,12 +1,18 @@
 import React from "react"
-import {Container} from "react-bootstrap";
+import {Container, Image} from "react-bootstrap";
 
 export function WelcomePage () {
     return (
         <>
-            <Container className="text-center">
+            <Container className="text-center mt-5">
                 <h1>Welcome, Furball!</h1>
             </Container>
+
+            <Container className="mt-5">
+            <Image fluid={true} className={"rounded-circle"} src={"https://www.placecage.com/200/200"} alt={"place cage image"} />
+            </Container>
+
+
         </>
     )
 }
