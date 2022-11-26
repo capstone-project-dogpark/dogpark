fa903020af8deff1a225517b9e5632a0c161b8a8
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-26 15:45:37 -0800

    changes to welcome page

--- frontend/src/ui/WelcomePage.jsx
+++ frontend/src/ui/WelcomePage.jsx
@@ -6,6 +6,8 @@ import Button from 'react-bootstrap/Button';
 export function WelcomePage () {
     return (
         <>
+
+
             <Container className="text-center mt-5">
                 <h1>Welcome, Furball!</h1>
                 <Image fluid={true} className={"rounded-circle"} src={"https://www.placecage.com/100/100"} alt={"place cage image"} />
