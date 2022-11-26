e7d183757c25f3281c15195e35bb9c7a6cc0f4da
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-26 11:48:30 -0800

    changes made to feed view component

--- frontend/src/ui/components/FeedView.jsx
+++ frontend/src/ui/components/FeedView.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import {Container, Row, Col} from "react-bootstrap";
 
 export function FeedView() {
     return (
         <>
+            <Container>
             <Card style={{ width: '30rem' }}>
                 <Card.Img variant="top" src="https://www.placecage.com/500/500" />
                 <Card.Body>
@@ -13,16 +15,16 @@ export function FeedView() {
                     <Card.Text>
                         Caption for post here.
                     </Card.Text>
+                    <Form style={{ width: '20rem' }}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label></Form.Label>
+                            <Form.Control type="email" placeholder="Comment" />
+                            <Button size="sm" className="mt-2" type="submit">Submit</Button>
+                        </Form.Group>
+                    </Form>
                 </Card.Body>
             </Card>
-            <Form style={{ width: '30rem' }}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="email" placeholder="Comment" />
-                    <Button className="mt-2" type="submit">Submit</Button>
-                </Form.Group>
-            </Form>
-
+            </Container>
         </>
     )
 
