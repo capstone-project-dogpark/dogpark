0086cdf633b75f6631b916d962859100b7a44318
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-26 09:35:53 -0800

    changed home link name

--- frontend/src/ui/components/Navbar.jsx
+++ frontend/src/ui/components/Navbar.jsx
@@ -10,7 +10,7 @@ export function Navigation() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nar">
                         <Nav className="ms-auto">
-                            <Nav.Link href='/'>Albuquerque Dog Parks</Nav.Link>
+                            <Nav.Link href='/'>Home</Nav.Link>
                             <Nav.Link href='/about'>The Community</Nav.Link>
                             <Nav.Link href='/SignUp'>Create a Pet Profile</Nav.Link>
                             <Nav.Link href='/Login'>Login</Nav.Link>
