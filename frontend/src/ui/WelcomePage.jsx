7b26c854ebf9c57aaa16adfd85fd5f23f8a27565
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-27 11:22:03 -0800

    corrected merge error for welcome page

--- frontend/src/ui/WelcomePage.jsx
+++ frontend/src/ui/WelcomePage.jsx
@@ -8,24 +8,16 @@ import {PostModal} from "./components/PostModal.jsx";
 export function WelcomePage () {
     return (
         <>
-
-
             <Container className="text-center mt-5">
                 <h1>Welcome, Furball!</h1>
                 <Image fluid={true} className={"rounded-circle"} src={"https://www.placecage.com/100/100"} alt={"place cage image"} />
             </Container>
 
-<<<<<<< HEAD
-            <Container className="mt-3 text-center mb-5">
-                                <Button size="sm" variant="primary">Create Post</Button>
-=======
             <Container className="mt-2 text-center mb-5">
-                                <PostModal/>
->>>>>>> welcome-page
+                <PostModal/>
             </Container>
 
             <FeedView/>
-
         </>
     )
 }
