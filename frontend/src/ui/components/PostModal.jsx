d62f552738714f62f5549d3eacf7dc7ca7c1a2c8
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-11-27 11:51:05 -0800

    deleted line of code that was no longer needed in post modal component

--- frontend/src/ui/components/PostModal.jsx
+++ frontend/src/ui/components/PostModal.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
-import {Container} from "react-bootstrap";
 
 export function PostModal() {
     const values = [''];
