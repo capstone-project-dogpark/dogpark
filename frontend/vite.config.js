9a79dcded08c6396730ab1c933ac5ae96ab04b4e
David Santos <dsantos4@cnm.edu>
2022-11-28 15:30:41 -0700

    added redux js directory & files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {setupProxy} from "./setupProxy.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css:{
     modules:{
-      localsConvention:"camelCase"
+      localsConvention:"camelCase",
+      '/apis': setupProxy
     }
   }
 })
