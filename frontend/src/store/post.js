033db1b94682120d3cffd2754a768dfba220bf53
David Santos <dsantos4@cnm.edu>
2022-11-28 15:58:40 -0700

    added post.js file

---
+++ frontend/src/store/post.js
@@ -0,0 +1,30 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {httpConfig} from "../utils/http-config.js";
+
+const slice = createSlice({
+    name: "posts",
+    initialState: [],
+    reducers: {
+        setAllPosts: (posts, action) => {
+            return action.payload
+        }
+    }
+})
+
+export const {setAllPosts} = slice.actions
+
+export const fetchAllPosts = () => async (dispatch) => {
+    const {data} = await httpConfig.get("/apis/post");
+    dispatch(setAllPosts(data));
+    let profileIdSet = new Set
+    for(let post of data){
+        const {postId, postProfileId} = post
+        if(profileIdSet.has(postProfileId) === false) {
+            profileIdSet.add(postProfileId)
+            dispatch(fetchProfileByProfileId(postProfileId))
+        }
+        dispatch(fetchLikesbyLikePostId(postId))
+    }
+};
+
+export default slice.reducer
