227cd086e27e39e647abce00ecdd0b88b3961c19
David Santos <dsantos4@cnm.edu>
2022-12-01 09:27:34 -0700

    added SignUpForm succesful test

--- frontend/src/ui/SignUp.jsx
+++ frontend/src/ui/SignUp.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Container, Row, Col} from "react-bootstrap";
+import {SignUpForm} from "./components/main-nav/sign-up/SignUpForm.jsx";
 
 export function SignUp () {
     return (
@@ -13,33 +14,34 @@ export function SignUp () {
             <Container>
                 <Row className="justify-content-center">
                     <Col className="col-8">
-            <Form>
-                <Form.Group controlId="enterEmail">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="dogsName">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="name" placeholder="Dog's Name" />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="createPassword">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="password" placeholder="Create a password" />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="confirmPassword">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="password" placeholder="Confirm password" />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
-                </Form.Group>
-                <Button className="mt-3" type="submit">Submit</Button>
-            </Form>
+                        <SignUpForm />
+{/*            <Form>*/}
+{/*                <Form.Group controlId="enterEmail">*/}
+{/*                    <Form.Label></Form.Label>*/}
+{/*                    <Form.Control type="email" placeholder="Enter email" />*/}
+{/*                    <Form.Text className="text-muted">*/}
+{/*                    </Form.Text>*/}
+{/*                </Form.Group>*/}
+{/*                <Form.Group controlId="dogsName">*/}
+{/*                    <Form.Label></Form.Label>*/}
+{/*                    <Form.Control type="name" placeholder="Dog's Name" />*/}
+{/*                    <Form.Text className="text-muted">*/}
+{/*                    </Form.Text>*/}
+{/*                </Form.Group>*/}
+{/*                <Form.Group controlId="createPassword">*/}
+{/*                    <Form.Label></Form.Label>*/}
+{/*                    <Form.Control type="password" placeholder="Create a password" />*/}
+{/*                    <Form.Text className="text-muted">*/}
+{/*                    </Form.Text>*/}
+{/*                </Form.Group>*/}
+{/*                <Form.Group controlId="confirmPassword">*/}
+{/*                    <Form.Label></Form.Label>*/}
+{/*                    <Form.Control type="password" placeholder="Confirm password" />*/}
+{/*                    <Form.Text className="text-muted">*/}
+{/*                    </Form.Text>*/}
+{/*                </Form.Group>*/}
+{/*                <Button className="mt-3" type="submit">Submit</Button>*/}
+{/*            </Form>*/}
                     </Col>
                 </Row>
             </Container>
