ce71ed3abfd341d6b3972419aa9a4545be5f7c2b
Celeste Rualizo <celeste.rualizo@yahoo.com>
2022-12-05 18:38:16 -0800

    added style to homepage

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -5,28 +5,20 @@ import {Comment} from "./components/Comment.jsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {ProfileCard} from "./components/ProfileCard";
 import {FeedView} from "./components/FeedView.jsx";
-
+import "./App.css"
 
 export function Home() {
     return (
         <>
-            <Container className="text-center mb-5">
-                <Row>
-                    <Col>
-                        <h1>Dogs Best Friend</h1>
-                        <h3>"Let's find a friend for your best friend"</h3>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="headerImage text-center">
+                        <h1>Dog's Best Friend</h1>
+                        <h6>Let's find a friend for your best friend</h6>
+            </div>
 
-            <Container className="container-fluid py-5">
-                <Row className="Row gx-md-5 gy-5">
-                    <Col className="text-center" md="7">
-                        <h2>Our Goal</h2>
-                        <p>Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.</p>
-                    </Col>
-                    <Col md="5">
-                        <Image fluid={true} className={"d-block mx-auto"} src={"https://www.placecage.com/gif/400/400"} alt={"place cage image"} />
+            <Container fluid className="container-fluid py-5">
+                <Row className="Row gx-md-2 gy-3">
+                    <Col className="text-center" md="12">
+                        <p>Welcome! Our mission is to provide dog owners with a place to interact with each other and share posts about their dogs and dog park experiences here in the city of Albuquerque. We have provided a map of all dog parks as well as the ability to create your own pet profile in order to share your experiences!</p>
                     </Col>
                 </Row>
 
