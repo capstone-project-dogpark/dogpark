9d6105177fb9d650bc3a7ad8d925a5c9a046f7eb
David Santos <dsantos4@cnm.edu>
2022-12-07 16:58:04 -0700

    freeze again

--- frontend/src/ui/MyFriendsPage.jsx
+++ frontend/src/ui/MyFriendsPage.jsx
@@ -5,17 +5,17 @@ import {useSelector} from "react-redux";
 import {fetchAuth} from "../store/auth.js";
 
 export function MyFriendsPage() {
-    const auth = useSelector((state => state.follows ? state.follows: []))
-    const follows = useSelector (state => state.follows ? state.follows: [])
-    const profile= useSelector(state => state.auth ?? null)
-    const dispatch = useDispatch ()
-
-    function initialEffects(){
-        dispatch(fetchAllFollows())
-        dispatch(fetchAuth())
-    }
-
-    useEffect(initialEffects, [dispatch])
+    // const auth = useSelector((state => state.follows ? state.follows: []))
+    // const follows = useSelector (state => state.follows ? state.follows: [])
+    // const profile= useSelector(state => state.auth ?? null)
+    // const dispatch = useDispatch ()
+    //
+    // function initialEffects(){
+    //     dispatch(fetchAllFollows())
+    //     dispatch(fetchAuth())
+    // }
+    //
+    // useEffect(initialEffects, [dispatch])
 
     return(
         <>
