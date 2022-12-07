4bbc980bc0bb69637f052d46182f1d9158cf0e33
David Santos <dsantos4@cnm.edu>
2022-12-07 16:56:02 -0700

    freeze

--- frontend/src/ui/MyFriendsPage.jsx
+++ frontend/src/ui/MyFriendsPage.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import {ProfileCard} from "./components/ProfileCard";
+import {useSelector} from "react-redux";
+import {fetchAuth} from "../store/auth.js";
 
 export function MyFriendsPage() {
+    const auth = useSelector((state => state.follows ? state.follows: []))
+    const follows = useSelector (state => state.follows ? state.follows: [])
+    const profile= useSelector(state => state.auth ?? null)
+    const dispatch = useDispatch ()
+
+    function initialEffects(){
+        dispatch(fetchAllFollows())
+        dispatch(fetchAuth())
+    }
+
+    useEffect(initialEffects, [dispatch])
+
     return(
         <>
             <Container className="text-center mt-5 mb-3" >
